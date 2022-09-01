80191512e2f5056df2803b160187e27b6808db27
RichCode <stanleyogada2000@gmail.com>
2022-09-01 13:46:37 +0100

    chore(test): setup environment
    - Have a dummy test passing to ensure a working test-env setup

---
+++ components/Cards/ProjectCard/ProjectCard.test.tsx
@@ -0,0 +1,7 @@
+describe("ProjectCard", () => {
+  test("renders without errors", () => {
+    expect(2).toBe(2);
+  });
+});
+
+export {};
---
+++ components/Cards/ProjectCard/ProjectCard.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export type ProjectCardProps = {};
+
+const ProjectCard = (props: ProjectCardProps) => {
+  return <div>ProjectCard</div>;
+};
+
+export default ProjectCard;
